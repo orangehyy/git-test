eabcb0b9a7eb51894450b918fe28845b23110b3d
hyy <1161775841@qq.com>
2022-03-02 15:15:53 +0800

    second commit

--- src/main/java/com/hyy/git/gitTest.java
+++ src/main/java/com/hyy/git/gitTest.java
@@ -3,5 +3,6 @@ package com.hyy.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello,git!");
+        System.out.println("Hello,git2!");
     }
 }
